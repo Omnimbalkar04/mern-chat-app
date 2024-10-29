d05e8784013a2da426f73c0ea4b8134d53f28763
Omnimbalkar04 <ompnimbalkar2004@gmail.com>
2024-10-29 21:27:53 +0530

    4th part completed- socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessages = async (req, res) => {
   try {
@@ -27,13 +28,19 @@ export const sendMessages = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    //Socket IO functionallity
-
     // await conversation.save();
     // await newMessage.save();
 
     //this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    //Socket IO functionallity
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      //this method is used to send events to specific clients
+      io.to(receiverSocketId).emit("newMessage",newMessage);
+    }
 
     res.status(201).json(newMessage);
 
--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -21,7 +22,7 @@ app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectMongoDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
