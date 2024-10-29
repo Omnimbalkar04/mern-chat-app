6cd2b084cf9f0401197a4a7ba3203e4911707533
Omnimbalkar04 <ompnimbalkar2004@gmail.com>
2024-10-29 23:03:19 +0530

    Deployment no 2

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+if(process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-})
+});
+}
 
 
 server.listen(PORT, () => {
